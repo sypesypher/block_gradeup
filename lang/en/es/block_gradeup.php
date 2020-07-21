fbe1a555526c514ecc10052b4bb377a97f9acbdd
Christopher Strothman <31223109+sypesypher@users.noreply.github.com>
2020-07-21 11:49:21 -0500

    add spanish files

---
+++ lang/en/es/block_gradeup.php
@@ -0,0 +1,20 @@
+<?php
+//TODO: add moodle comment garbage discalaimers
+
+$string['pluginname'] = 'Gradeup';
+$string['gradeup'] = 'Gradeup';
+$string['gradeup:addinstance'] = 'Add a new Gradeup block';
+$string['gradeup:myaddinstance'] = 'Add a new Gradeup block to the My Moodle page';
+$string['promptString'] = 'Por favor, introduzca una calificación';
+$string['gradeString'] = 'Grado';
+$string['weightString'] = 'Peso'; //someone check this?
+$string['averageString'] = 'Promedio';
+$string['maxGradeString'] = 'Grado Máximo';
+$string['projectionGrade'] = 'Proyección de grado';
+$string['legendMin'] = 'Tu calificación actual/Grado mínimo';
+$string['legendAverage'] = 'Promedio de clase';
+$string['potentialGradeString'] = 'Su potencial grado/Grado Maximo';
+$string['legendPredict'] = 'Tu calificación pronosticada';
+$string['legendLabel'] = 'Leyenda';
+$string['resetButton'] = 'Reiniciar datos';
+$string['heatmapLabel'] = 'Mapa de calor';
