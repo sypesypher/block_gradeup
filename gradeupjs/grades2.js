1ccdd858922fa54c8591c9363fd7e6864186999b
Christopher Strothman <31223109+sypesypher@users.noreply.github.com>
2020-08-05 07:41:53 -0500

    add grades data to match new formatting and for new heatmap with SVG.js

---
+++ gradeupjs/grades2.js
@@ -0,0 +1,33 @@
+let grades = [
+{
+	itemname: "HW1",
+	weight: 5,
+	score: 0.7,
+	originalScore: 0.7,
+	averageScore: 0.8,
+	due: 1592352000
+	},
+	{itemname: "Hw2",weight: 5,score: 0.7,originalScore: 0.7,averageScore: 0.8,due: 1593475200
+	},
+	{itemname: "Exam2",weight: 20,score: 0.7,originalScore: 0.7,averageScore: 0.8,due: 1595808000
+	},
+	{itemname: "Hw3",weight: 5,score: 0.7,originalScore: 0.7,averageScore: 0.8,due: 1597536000
+	},
+	{itemname: "Hw4",weight: 5,score: 0.7,originalScore: 0.7,averageScore: 0.8,due: 1597795200
+	},
+	{itemname: "Exam1",weight: 20,score: 0.7,originalScore: 0.7,averageScore: 0.8,due: 1599004800
+	},
+	{itemname: "Hw5",weight: 5,score: 0.7,originalScore: 0.7,averageScore: 0.8,due: 1600128000
+	},
+	{itemname: "Hw6",weight: 5,score: 0.7,originalScore: 0.7,averageScore: 0.8,due: 1602892800
+	},
+	{itemname: "Missing1",weight: 15,score: null,originalScore: null,averageScore: null,due: 1604966400
+	},
+	{itemname: "Missing2"
+	,weight: 15,
+	score: null,
+	originalScore: null,
+	averageScore: null,
+	due: 1606867200
+	}
+	];
