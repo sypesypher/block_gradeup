1b3e541065f8510863366ff35f66b088bcd3bc6e
Christopher Strothman <31223109+sypesypher@users.noreply.github.com>
2020-08-07 11:04:53 -0500

    Delete sumArray.js

--- sumArray.js
+++
@@ -1,9 +0,0 @@
-function sumArray(arrData) {
-    var total = 0;
-    for (var i = 0; i < arrData.length; i++){
-        total += arrData[i];
-    }
-    return total;
-}
-
-module.exports = sumArray 
