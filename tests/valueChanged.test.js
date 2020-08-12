969120ed84b39f307da2bdc3012b51d3fd33909f
Christopher Strothman <31223109+sypesypher@users.noreply.github.com>
2020-08-12 11:41:47 -0500

    required tests

--- tests/valueChanged.test.js
+++ tests/valueChanged.test.js
@@ -1,8 +1,11 @@
 //How to run the tests: npx jest test --watchAll
 //from the top level directory
 
-const sumArray = require('../gradeupjs/valueChanged.js')
+//TODO: does this still work after removing const sumarray =     ???? if yes, repeat on other tests
+require('../gradeupjs/valueChanged.js')
 
 test("default test true", () => {
     expect(true).toBeTruthy();
 })
+
+//test valuechanged
