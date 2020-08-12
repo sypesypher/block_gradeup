392060a8f09ff9355f64f4362e1b7eedad547380
Christopher Strothman <31223109+sypesypher@users.noreply.github.com>
2020-08-12 11:38:49 -0500

    tests required

--- tests/burnup.test.js
+++ tests/burnup.test.js
@@ -6,3 +6,11 @@ const sumArray = require('../gradeupjs/burnup.js')
 test("default test true", () => {
     expect(true).toBeTruthy();
 })
+
+//test DrawAssignment
+//test drawPrediction
+//test drawAverageGrades
+//test drawText somehow?
+//test drawLegend
+//test drawAssignments
+//test drawchart
